50d3ea6249f444720f5dbf973e2be15a2262034a
Mit Gandhi <gandhimit04@gmail.com>
2025-10-07 23:23:43 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section 
       id="home" 
       className="relative bg-cover bg-center bg-no-repeat text-white"
-      style={{ backgroundImage: 'url("public/image.png")', backgroundPosition: 'center center' }}
+      style={{ backgroundImage: 'url("/image.png")', backgroundPosition: 'center center' }}
     >
       {/* Overlay for better text readability */}
       <div className="absolute inset-0 bg-black bg-opacity-40"></div>
