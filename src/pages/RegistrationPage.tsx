56d0daf6e358798caa1295d303b9f1c599a07c30
Mit Gandhi <gandhimit04@gmail.com>
2025-10-07 23:25:41 +0530

    Update RegistrationPage.tsx

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -7,7 +7,7 @@ const RegistrationPage = () => {
     <div>
       <section 
         className="relative bg-cover bg-center bg-no-repeat py-16"
-        style={{ backgroundImage: 'url(/public/image.png)', backgroundPosition: 'center center' }}
+        style={{ backgroundImage: 'url("/image.png")', backgroundPosition: 'center center' }}
       >
         {/* Overlay for better text readability */}
         <div className="absolute inset-0 bg-black bg-opacity-40"></div>
